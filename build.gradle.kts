694e275fdf672bb6c9187b09c250fd7e189ff692
limbang <495071565@qq.com>
2022-03-18 20:19:51 +0800

    v2.0.2 版本 1.添加模组作者或团队显示

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "top.limbang"
-version = "2.0.1"
+version = "2.0.2"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
--- src/main/kotlin/top/limbang/mcmod/mirai/McmodPlugin.kt
+++ src/main/kotlin/top/limbang/mcmod/mirai/McmodPlugin.kt
@@ -27,7 +27,7 @@ import top.limbang.mcmod.network.model.SearchFilter
 object McmodPlugin : KotlinPlugin(
     JvmPluginDescription(
         id = "top.limbang.mirai-console-mcmod-plugin",
-        version = "2.0.1",
+        version = "2.0.2",
     ) {
         author("limbang")
         info("""mc百科查询""")
