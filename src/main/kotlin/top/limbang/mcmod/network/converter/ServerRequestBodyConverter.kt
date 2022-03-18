e8269b02893f5851e9dc1611f4b25b99daee4ebc
limbang <495071565@qq.com>
2022-03-18 10:32:04 +0800

    style: 添加版权信息

--- src/main/kotlin/top/limbang/mcmod/network/converter/ServerRequestBodyConverter.kt
+++ src/main/kotlin/top/limbang/mcmod/network/converter/ServerRequestBodyConverter.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2020-2022 limbang and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/limbang/mirai-console-mcmod-plugin/blob/master/LICENSE
+ */
+
 package top.limbang.mcmod.network.converter
 
 import okhttp3.FormBody
--- src/main/kotlin/top/limbang/mcmod/network/converter/ServerResponseBodyConverter.kt
+++ src/main/kotlin/top/limbang/mcmod/network/converter/ServerResponseBodyConverter.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2020-2022 limbang and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/limbang/mirai-console-mcmod-plugin/blob/master/LICENSE
+ */
+
 package top.limbang.mcmod.network.converter
 
 import okhttp3.ResponseBody
@@ -10,7 +19,7 @@ import top.limbang.mcmod.network.utils.parse
  * ### 把 html 响应解析成 [Server]
  */
 class ServerResponseBodyConverter : Converter<ResponseBody, Server> {
-    override fun convert(value: ResponseBody): Server? {
+    override fun convert(value: ResponseBody): Server {
         val document = value.parse()
         val elements = document.select(".server-info-ext > li > span")
         return Server(
